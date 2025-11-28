636b962ccc57c0fd08191e146dcb86bbb1248eb6
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-28 22:55:34 +0300

    Fix(regex,Dict): the regex error and json converting Fixed

--- task1.py
+++ task1.py
@@ -1,10 +1,38 @@
 import re 
 from collections import Counter
 
-text = "The quick brown fox jumps over the lazy dog the fox"
+def text_analyze(text):
 
-words = re.findall(r"[A-za-z]+(?:'[A-Za-z]+)?", text.lower())
-length_counts = Counter(len(word) for word in words)
+    #Clean split text regex
+    words = re.findall(r"[A-za-z]+(?:'[A-za-z]+)?", text.lower())
 
-for n,freq in sorted(length_counts.items()):
-    print("%d -> %d" % (n,freq))
+
+    #Handle Empty Cases
+    if not words:
+        return { "word_count":0, "average_word_length": 0.000}
+
+    #Average word length(2 decimals)
+    total_characterstics = sum(len(w) for w in words )
+    average_word_length = total_characterstics / len(words)
+    formatted_averager = float(f"{average_word_length:.2f}") #an algorithm to round it
+
+    #longest word printer
+    max_length = max(len(w) for w in words)
+    longest_words = [w for w in words if len(w) == max_length]
+   
+
+#word Frequenxy counter
+    word_frequency = Counter(words)
+    return {
+        "word_count": len(words),
+        "average_word_length": formatted_averager,
+        "longest_words": longest_words,
+        "word_frequency": dict(word_frequency)
+
+    }
+
+#input
+input_text = "The quick brown fox jumps over the lazy dog the fox"
+input_text2 = "This is another test code to count"
+print(text_analyze(input_text))
+print(text_analyze(input_text2))
