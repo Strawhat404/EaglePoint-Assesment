cf59b81ea151097021e849e8e0b98f03aaaa2d9b
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-29 03:15:59 +0300

    Feat(Math.random): to simulate failure rate math.random function implemented

--- task2.js
+++ task2.js
@@ -1,21 +1,28 @@
-//the wait function combines Promise + siteTimeout
+//define a function that returns a promise that resolves after a delay
+
+//the wait function combines Promise + siteTimeout to create an async delay
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+//this is mock function that simulates API calls with network delays and random failure 
 async function mock(url) {
     console.log('Fetching data from', url);
 
     //network delay it makes it wait 0.5 seconds
+    //this means the async function pause execution using await
     await wait(500); // Simulate network delay
     
     //Chance of failure
-    const randomHappen = Math.random();
+    //generate random number between 0 and 1 for success or failure
+    const randomHappen = Math.random();//returns floating number
 
+    //if random number is less than 0.7 or 70% simulates a network failure
     if (randomHappen <0.7){
         throw new Error ('Network Error Occurred');
 
     }
-    else {
+    else { //if succes THIS RETURNA A MOCK RESPONSE OBJECT
         return{
-            status: 200,
+            status: 200,//http status code for ok
             data: {
                 message: 'Data fetched successfully from ' }
             };
@@ -48,16 +55,19 @@ async function mock(url) {
 
 }
 
-//async wrapper to run the code
-
+//async wrapper to use wair
+//this is the main block that runs when the script loads
 (async () => {
     console.log("starting task test");
 
     try{
+        //attempting to fetch data from the given url 
+        //which is maximum 5 retry attempting
         const data = await fetchDatawithRetry('https://api.eaglepoint.yoseph.com/data', 5);
         console.log('"\n final Value reult:', data);  
 
     }catch(finalError){
+        //if all retries falls this catches the final error and log
         console.log("\n final Error:", finalError.message); 
     }
     })();
